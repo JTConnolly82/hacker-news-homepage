c4056534e5e1aed2b696f20eef6a25318904bafb
john connolly <jtconnolly82@gmail.com>
2019-10-21 18:45:48 -0600

    layout page 1

--- app.js
+++ app.js
@@ -7,21 +7,43 @@ axios.get('https://hacker-news.firebaseio.com/v0/topstories.json').then((res)=>{
     let author = res.data.by;
     let title = res.data.title;
     let url = res.data.url;
-    let storyWrap = document.createElement('span');
+    let storyWrap = document.createElement('div');
+    storyWrap.className = 'storywrap';
+    let arrows = document.createElement('div');
+    arrows.className = 'arrows';
+    let upvote = document.createElement('div');
+    upvote.innerText = '👆'
+    upvote.className = 'upvote';
+    arrows.appendChild(upvote);
+    let downvote = document.createElement('div');
+    downvote.innerText = '👇'
+    downvote.className = 'downvote';
+    arrows.appendChild(downvote);
+    let scoreBar = document.createElement('div');
+    scoreBar.innerText = res.data.score;
+    scoreBar.className = 'scorebar'
+    let endDiv = document.createElement('div');
+    endDiv.className = 'enddiv'
     let storyTitle = document.createElement('h2');
     storyTitle.innerText = title;
+    storyTitle.className = 'storytitle'
     let storyAuth = document.createElement('h3');
-    storyAuth.innerText = author;
-    let storyUrl = document.createElement('h4');
-    storyUrl.innerText = url;
+    storyAuth.className = 'storyauth';
+    storyAuth.innerText = '👤 ' + author;
+    let storyUrl = document.createElement('a');
+    storyUrl.className = 'storyurl';
+    storyUrl.href = url;
+    storyUrl.innerText = 'Link To Story';
+    endDiv.appendChild(storyAuth);
+    endDiv.appendChild(storyUrl);
+    storyWrap.appendChild(arrows);
+    storyWrap.appendChild(scoreBar);
     storyWrap.appendChild(storyTitle);
-    storyWrap.appendChild(storyAuth);
-    storyWrap.appendChild(storyUrl);
+    storyWrap.appendChild(endDiv);
     storiesDiv.appendChild(storyWrap);
     });
   });
 });
-
 
 
 
