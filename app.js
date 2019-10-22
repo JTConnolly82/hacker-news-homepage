708dc4a96ccefac3612757f44f95dc090cae81da
john connolly <jtconnolly82@gmail.com>
2019-10-22 11:59:51 -0600

    commented out comment button code

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ axios.get('https://hacker-news.firebaseio.com/v0/topstories.json').then((res)=>{
   let stories = res.data.slice(0, 30);
   stories.forEach(element => {
     axios.get("https://hacker-news.firebaseio.com/v0/item/" + element + ".json").then((res) => {
+  // pull author title url for displaying in innerText
     let author = res.data.by;
     let title = res.data.title;
-    let url = res.data.url;
+    let url = res.data.url; 
+  // creating DOM elements, adding innnerText using vars above
     let storyWrap = document.createElement('div');
     storyWrap.className = 'storywrap';
     let arrows = document.createElement('div');
@@ -22,7 +24,23 @@ axios.get('https://hacker-news.firebaseio.com/v0/topstories.json').then((res)=>{
     endDiv.className = 'enddiv'
     let storyTitle = document.createElement('h2');
     storyTitle.innerText = title;
-    storyTitle.className = 'storytitle'
+    storyTitle.className = 'storytitle';
+    // let commentLink = document.createElement('button');
+    // commentLink.className = 'commentlink';
+    // commentLink.innerText = 'Comments';
+    // commentLink.addEventListener('click', function () {
+    //   let kidsOnTheBlock = [];
+    //   for (let i = 0; i < res.data.kids.length; i++) {
+    //       axios.get("https://hacker-news.firebaseio.com/v0/item/" + res.data.kids[i] + ".json").then(  
+    //       (getKid) => kidsOnTheBlock.push(getKid));
+    //   }
+    //   console.log(kidsOnTheBlock);
+    //   for (let i = 0; i < kidsOnTheBlock.length; i++) {
+    //     console.log(kidsOnTheBlock[i]);
+    //   }      
+    // });
+
+
     let storyAuth = document.createElement('h3');
     storyAuth.className = 'storyauth';
     storyAuth.innerText = '👤 ' + author;
@@ -30,16 +48,23 @@ axios.get('https://hacker-news.firebaseio.com/v0/topstories.json').then((res)=>{
     storyUrl.className = 'storyurl';
     storyUrl.href = url;
     storyUrl.innerText = 'Link To Story';
-    endDiv.appendChild(storyAuth);
-    endDiv.appendChild(storyUrl);
-    storyWrap.appendChild(arrows);
-    storyWrap.appendChild(scoreBar);
-    storyWrap.appendChild(storyTitle);
-    storyWrap.appendChild(endDiv);
-    storiesDiv.appendChild(storyWrap);
+
+  // append all the elements
+    if (document.baseURI === 'file:///Users/john/Desktop/myprojects/hacker-news-homepage/index.html') {
+      endDiv.appendChild(storyAuth);
+      endDiv.appendChild(storyUrl);
+      storyWrap.appendChild(arrows);
+      storyWrap.appendChild(scoreBar);
+      storyWrap.appendChild(storyTitle);
+      // storyWrap.appendChild(commentLink);
+      storyWrap.appendChild(endDiv);
+      storiesDiv.appendChild(storyWrap);
+    }
     });
   });
 });
+
+
 
 
 
